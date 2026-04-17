bc6b677ed63516802fca5c50dd3615133e76428a
varnagycsanad <cvarnagy@gmail.com>
2026-04-17 21:11:15 +0200

    fix(build): set /ujsite/ base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // [CHANGE 2026-04-17 20:47] Convert project to static-only Astro build
+  // [CHANGE 2026-04-17 21:06] Set base path for /ujsite/ static deploy
+  base: '/ujsite/',
+  output: 'static',
+});
